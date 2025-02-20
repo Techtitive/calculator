a67e718b0038addc6b1f6367290fb545588a8643
Techtitive <151868617+Techtitive@users.noreply.github.com>
2025-02-20 20:06:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function appendtodisplay(input){
     inputdisp.value += input;
 };
 
+document.addEventListener("keydown", (event) => {
+    const allowedKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "."];
+
+    if (allowedKeys.includes(event.key)) {
+        appendtodisplay(event.key);
+    }
+    else if (event.key === "Enter"){
+        equal()
+    }
+});
+
+
 function equal(){
     const answer = parseFloat(eval(inputdisp.value).toFixed(2));
     console.log(answer)
@@ -24,4 +36,46 @@ erase.addEventListener("click", () => {
     setTimeout(() => {
         inputdisp.style.boxShadow = "none";
     }, 3000);
+});
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace"){
+        inputdisp.value = inputdisp.value.slice(0, -1);
+        inputdisp.style.boxShadow = "0px 0px 5px yellow, 0px 0px 12px yellow"; 
+
+        setTimeout(() => {
+            inputdisp.style.boxShadow = "none";
+        }, 3000);
+    }
+
+
+});
+
+let eraseHoldTimeout;
+let keyPressStart = {};
+
+erase.addEventListener("mousedown", () => {
+    eraseHoldTimeout = setTimeout(() => {
+        inputdisp.value = "";
+    }, 1000);
+});
+
+erase.addEventListener("mouseup", () => clearTimeout(eraseHoldTimeout));
+erase.addEventListener("mouseleave", () => clearTimeout(eraseHoldTimeout));
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace" && !keyPressStart[event.key]) {
+        keyPressStart[event.key] = Date.now();
+
+        eraseHoldTimeout = setTimeout(() => {
+            inputdisp.value = "";
+        }, 1000);
+    }
+});
+
+document.addEventListener("keyup", (event) => {
+    if (event.key === "Backspace") {
+        clearTimeout(eraseHoldTimeout);
+        delete keyPressStart[event.key];
+    }
 });
